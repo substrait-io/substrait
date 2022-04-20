9d8b318b405661f9eb6efe9c99dfef748b290857
Matthew Topol <mtopol@factset.com>
2022-04-20 13:52:07 -0400

    update buf.gen.yaml, update proto files

--- go/substrait/doc.go
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-
-// Package substrait provides experimental Go bindings for
-// https://substrait.io.
-package substrait
-
-//go:generate ./gen-proto.sh
--- go/substrait/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/substrait-io/substrait/go/substrait
-
-go 1.16
-
-require google.golang.org/protobuf v1.28.0
