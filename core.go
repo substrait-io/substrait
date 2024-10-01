3dca9b505d8122542de311a7d21973b74b58b761
anshuldata <anshul@sundeck.io>
2024-10-01 06:34:18 +0530

    feat: make substrait repo a go module (#712)
    This facilitates substrait assets use in go projects

---
+++ core.go
@@ -0,0 +1,14 @@
+// Package substrait provides access to Substrait artifacts via embed.FS.
+// Use substrait.GetSubstraitFS() to retrieve the embed.FS object.
+package substrait
+
+import "embed"
+
+// Add all directories which should be exposed in below
+//
+//go:embed extensions/*
+var substraitFS embed.FS
+
+func GetSubstraitFS() embed.FS {
+	return substraitFS
+}
