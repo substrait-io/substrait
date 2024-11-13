985a2ed85c382586edef0edd9fd955748946e0b4
Chandra Sanapala <chandra@sundeck.io>
2024-11-14 04:39:28 +0530

    review comments: add new api GetSubstraitTestsFS

--- core.go
+++ core.go
@@ -4,12 +4,20 @@ package substrait
 
 import "embed"
 
-// Add all directories which should be exposed in below
-//
 //go:embed extensions/*
-//go:embed tests/cases/*/*
-var substraitFS embed.FS
+var substraitExtensionsFS embed.FS
 
 func GetSubstraitFS() embed.FS {
-	return substraitFS
+	return substraitExtensionsFS
+}
+
+func GetSubstraitExtensionsFS() embed.FS {
+	return substraitExtensionsFS
+}
+
+//go:embed tests/cases/*/*.test
+var substraitTestsFS embed.FS
+
+func GetSubstraitTestsFS() embed.FS {
+	return substraitTestsFS
 }
--- core_test.go
+++ core_test.go
@@ -9,19 +9,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetSubstraitFS(t *testing.T) {
-	got := GetSubstraitFS()
+func TestGetSubstraitExtensionsFS(t *testing.T) {
+	fsArr := []embed.FS{GetSubstraitExtensionsFS(), GetSubstraitFS()}
+	for _, got := range fsArr {
+		filePaths, err := ListFiles(got, ".")
+		require.NoError(t, err)
+		assert.Greater(t, len(filePaths), 15)
+		assert.Contains(t, filePaths, "extensions/functions_arithmetic.yaml")
+		assert.Contains(t, filePaths, "extensions/functions_arithmetic_decimal.yaml")
+		assert.Contains(t, filePaths, "extensions/functions_datetime.yaml")
+	}
+}
+
+func TestGetSubstraitTestsFS(t *testing.T) {
+	got := GetSubstraitTestsFS()
 	filePaths, err := ListFiles(got, ".")
 	require.NoError(t, err)
-	assert.Greater(t, len(filePaths), 19)
+	assert.Greater(t, len(filePaths), 3)
 	assert.Contains(t, filePaths, "tests/cases/arithmetic/add.test")
 	assert.Contains(t, filePaths, "tests/cases/arithmetic/max.test")
 	assert.Contains(t, filePaths, "tests/cases/arithmetic_decimal/power.test")
 	assert.Contains(t, filePaths, "tests/cases/datetime/lt_datetime.test")
-
-	assert.Contains(t, filePaths, "extensions/functions_arithmetic.yaml")
-	assert.Contains(t, filePaths, "extensions/functions_arithmetic_decimal.yaml")
-	assert.Contains(t, filePaths, "extensions/functions_datetime.yaml")
 }
 
 func ListFiles(embedFs embed.FS, root string) ([]string, error) {
