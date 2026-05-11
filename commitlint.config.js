adb964ae114c63f094f49f5225509a8472b9989f
Niels Pardon <par@zurich.ibm.com>
2026-05-11 13:31:24 +0200

    ci: switch from dependabot to renovate
    Signed-off-by: Niels Pardon <par@zurich.ibm.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  // Workaround for https://github.com/dependabot/dependabot-core/issues/5923
+  // Bot PR descriptions are not conventional-commit bodies, so don't lint them.
   ignores: [
+    // Dependabot, see https://github.com/dependabot/dependabot-core/issues/5923.
+    // Kept while dependabot PRs opened before the switch to renovate are still open.
     (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
     (message) => /^Updates the requirements on .+ to permit the latest version\.$/m.test(message),
+    // Renovate
+    (message) => /^This PR contains the following updates:$/m.test(message),
   ],
   rules: {
     "body-max-line-length": [0, "always", Infinity],
