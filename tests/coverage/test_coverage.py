04b05f5ad68bd673a8fe464278a0f29f4025f343
Ben Bellick <ben.bellick@datadoghq.com>
2026-09-24 16:33:31 +0000

    test(parser): move test_parse_aggregate_func_test_compact

--- tests/coverage/test_coverage.py
+++ tests/coverage/test_coverage.py
@@ -72,15 +72,6 @@ def make_window_test_header(version, include):
 
 
 
-def test_parse_aggregate_func_test_compact():
-    header = make_aggregate_test_header(
-        "v1.0", "extension:io.substrait:functions_arithmetic"
-    )
-    tests = """# basic
-((20, 20), (-3, -3), (1, 1), (10,10), (5,5)) corr(col0::fp32, col1::fp32) = 1::fp64
-"""
-    test_file = parse_string(header + tests)
-    assert len(test_file.testcases) == 1
 
 
 def test_parse_aggregate_func_test_multiple_args():
