fc58828ce110a1399c389c2a9e25bfc3bd1e6059
Ben Bellick <ben.bellick@datadoghq.com>
2026-09-24 16:33:36 +0000

    test(parser): move test_parse_window_func_test_empty_frame

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -499,3 +499,18 @@ lag(col0, 2::i32, 5::i32) OVER f1 = (5, 5, 1998, 1999)::i32?
     assert test_file.testcases[0].result == CaseLiteral(
         ["5", "5", "1998", "1999"], "i32?", nullable=True
     )
+
+
+def test_parse_window_func_test_empty_frame():
+    header = make_window_test_header(
+        "v1.0", "extension:io.substrait:functions_arithmetic"
+    )
+    tests = """# empty frame
+DEFINE f1(i32) = ()
+row_number() OVER f1 = ()::i64?
+"""
+    test_file = parse_string(header + tests)
+    assert len(test_file.testcases) == 1
+    assert test_file.testcases[0].func_name == "row_number"
+    assert test_file.testcases[0].rows == []
+    assert test_file.testcases[0].result == CaseLiteral([], "i64?", nullable=True)
