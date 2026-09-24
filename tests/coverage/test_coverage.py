b66a289859463704daaa09c1a2911a4af730a9cc
Ben Bellick <ben.bellick@datadoghq.com>
2026-09-24 16:33:33 +0000

    test(parser): move test_parse_aggregate_func_test_compact_string_agg

--- tests/coverage/test_coverage.py
+++ tests/coverage/test_coverage.py
@@ -78,16 +78,6 @@ def make_window_test_header(version, include):
 
 
 
-def test_parse_aggregate_func_test_compact_string_agg():
-    header = make_aggregate_test_header(
-        "v1.0", "extension:io.substrait:functions_arithmetic"
-    )
-    tests = """# basic
-(('ant'), ('bat'), ('cat')) string_agg(col0::str, ','::str) = 1::fp64
-(('ant'), ('bat'), ('cat')) string_agg(col0::string, ','::string) = 1::fp64
-"""
-    test_file = parse_string(header + tests)
-    assert len(test_file.testcases) == 2
 
 
 def test_parse_aggregate_func_max():
