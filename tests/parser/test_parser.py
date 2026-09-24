9485e8de1d764bde8e90ec7aa598f1ef8256903b
Ben Bellick <ben.bellick@datadoghq.com>
2026-09-24 16:33:28 +0000

    test(parser): move test_parse_nullable_user_defined_type_literal

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -340,3 +340,18 @@ some_func((4, 2)::u!point) = (1, 1)::u!point
     assert len(test_file.testcases) == 1
     assert test_file.testcases[0].args[0] == CaseLiteral(["4", "2"], "u!point")
     assert test_file.testcases[0].result == CaseLiteral(["1", "1"], "u!point")
+
+
+def test_parse_nullable_user_defined_type_literal():
+    header = make_header("v1.0", "extension:org.example:extension_types")
+    tests = """# basic
+some_func((4, 2)::u!point?) = (1, 1)::u!point?
+"""
+    test_file = parse_string(header + tests)
+    assert len(test_file.testcases) == 1
+    assert test_file.testcases[0].args[0] == CaseLiteral(
+        ["4", "2"], "u!point?", nullable=True
+    )
+    assert test_file.testcases[0].result == CaseLiteral(
+        ["1", "1"], "u!point?", nullable=True
+    )
