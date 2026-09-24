c95336d6ab2114c6bb9980e1eae37061ca595e92
Ben Bellick <ben.bellick@datadoghq.com>
2026-09-24 16:33:16 +0000

    test(parser): move test_parse_basic_example

--- tests/coverage/test_coverage.py
+++ tests/coverage/test_coverage.py
@@ -34,19 +34,6 @@ def make_window_test_header(version, include):
 """
 
 
-def test_parse_basic_example():
-    header = make_header("v1.0", "extension:io.substrait:functions_arithmetic")
-    tests = """# 'Basic examples without any special cases'
-add(120::i8, 5::i8) = 125::i8
-add(100::i16, 100::i16) = 200::i16
-add(1::i8?, 2::i8?) = 3::i8?
-
-# Overflow examples demonstrating overflow behavior
-add(120::i8, 10::i8) [overflow:ERROR] = <!ERROR>
-"""
-
-    test_file = parse_string(header + tests)
-    assert len(test_file.testcases) == 4
 
 
 def test_parse_func_call_arg():
--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -35,3 +35,18 @@ def make_window_test_header(version, include):
 def get_absolute_path(relative_path):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(script_dir, relative_path)
+
+
+def test_parse_basic_example():
+    header = make_header("v1.0", "extension:io.substrait:functions_arithmetic")
+    tests = """# 'Basic examples without any special cases'
+add(120::i8, 5::i8) = 125::i8
+add(100::i16, 100::i16) = 200::i16
+add(1::i8?, 2::i8?) = 3::i8?
+
+# Overflow examples demonstrating overflow behavior
+add(120::i8, 10::i8) [overflow:ERROR] = <!ERROR>
+"""
+
+    test_file = parse_string(header + tests)
+    assert len(test_file.testcases) == 4
